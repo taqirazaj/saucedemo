5f600fb05f5d89279a31cb04548904fa9c204267
taqirazaj <muhammadtaaqiraza@gmail.com>
2024-09-07 13:37:05 +0500

    Reverted

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('cypress');
-const cypressMochawesomeReporter = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
   projectId: '2asdmr',
@@ -8,15 +7,7 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000, // 10 seconds
     pageLoadTimeout: 120000, // 2 minutes
     setupNodeEvents(on, config) {
-      // Integrate the reporter plugin
-      cypressMochawesomeReporter(on);
-      // Implement other node event listeners here
-    },
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      reportDir: 'cypress/reports', // Directory to save reports
-      html: true,
-      json: true,
     },
   },
-});
+},
+);
