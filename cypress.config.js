d4ffaea4a0fbf7e2258165aa86633e72b986cd43
taqirazaj <muhammadtaaqiraza@gmail.com>
2024-09-07 13:03:30 +0500

    Added Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const { reporter } = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
   projectId: "2asdmr",
@@ -7,7 +8,13 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000, // 10 seconds
     pageLoadTimeout: 120000, // 2 minutes
     setupNodeEvents(on, config) {
+      reporter(on); // Integrate the reporter plugin
     },
-   
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports', // Directory to save reports
+      html: true,
+      json: true,
+    },
   },
 });
